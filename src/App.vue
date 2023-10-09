b3e4be96425a92efc398fe442956eeb053eb014a
aleroms <santiagomorales333@yahoo.com>
2023-10-09 11:17:58 -0600

    adding task component card

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,24 @@
       <img src="./assets/Pinialogo.svg" alt="pinia" />
       <h1>Pinia Tasks</h1>
     </header>
+
+    <!-- task list  -->
+    <div class="task-list" v-for="task in taskStore.tasks">
+      <TaskDetails :task="task" />
+    </div>
   </main>
 </template>
 
 <script>
-export default {};
+import TaskDetails from "@/components/TaskDetails.vue";
+import { useTaskStore } from "@/stores/taskStore.js";
+export default {
+  components: { TaskDetails },
+  setup() {
+    //returns stores
+    const taskStore = useTaskStore();
+
+    return { taskStore };
+  },
+};
 </script>
