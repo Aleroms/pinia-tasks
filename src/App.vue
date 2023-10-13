9fc2a5ad08d9f12653c8a6ae2505188c72f4bd69
aleroms <santiagomorales333@yahoo.com>
2023-10-13 15:03:13 -0600

    created 3 getter functions for tasks

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,26 @@
       <h1>Pinia Tasks</h1>
     </header>
 
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">all tasks</button
+      ><button @click="filter = 'favs'">favorites tasks</button>
+    </nav>
+
     <!-- task list  -->
-    <div class="task-list" v-for="task in taskStore.tasks">
-      <TaskDetails :task="task" />
+    <div class="task-list" v-if="filter === 'all'">
+      <p>You have {{ taskStore.totalCount }} tasks left to do</p>
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <!-- looping over filtered favorites in taskStore() -->
+    <div class="task-list" v-if="filter === 'favs'">
+      <p>You have {{ taskStore.favCount }} favs left to do</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task" />
+      </div>
     </div>
   </main>
 </template>
@@ -16,13 +33,15 @@
 <script>
 import TaskDetails from "@/components/TaskDetails.vue";
 import { useTaskStore } from "@/stores/taskStore.js";
+import { ref } from "vue";
 export default {
   components: { TaskDetails },
   setup() {
     //returns stores
     const taskStore = useTaskStore();
+    const filter = ref("all");
 
-    return { taskStore };
+    return { taskStore, filter };
   },
 };
 </script>
--- src/stores/TaskStore.js
+++ src/stores/TaskStore.js
@@ -4,7 +4,24 @@ export const useTaskStore = defineStore("taskStore", {
   state: () => ({
     tasks: [
       { id: 1, title: "buy some milk", isFav: false },
-      { id: 2, title: "play game", isFav: false },
+      { id: 2, title: "play game", isFav: true },
     ],
   }),
+  getters: {
+    favs() {
+      return this.tasks.filter((t) => t.isFav);
+    },
+    favCount() {
+      //reduce returns a value
+      return this.tasks.reduce((prev, cur) => {
+        return cur.isFav ? prev + 1 : prev;
+      }, 0);
+      //0 is initial value for p
+    },
+    totalCount: (state) => {
+      return state.tasks.length;
+    },
+  },
 });
+//getters can manipulate data before returning the values
+//returns new values and does not manipulate state
