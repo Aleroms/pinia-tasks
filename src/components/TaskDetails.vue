0902fa4bca9dd4e5514e5b8b2e8cd4468b0bcf65
aleroms <santiagomorales333@yahoo.com>
2023-10-16 22:47:23 -0600

    added delete and toggle actions to pinia

--- src/components/TaskDetails.vue
+++ src/components/TaskDetails.vue
@@ -2,15 +2,30 @@
   <div class="task">
     <h3>{{ task.title }}</h3>
     <div class="icons">
-      <i class="material-icons">delete</i>
-      <i class="material-icons">favorite</i>
+      <i class="material-icons" @click="taskStore.deleteTask(task.id)"
+        >delete</i
+      >
+      <!-- applies active class if isFav is true -->
+      <i
+        :class="{ active: task.isFav }"
+        class="material-icons"
+        @click="taskStore.toggleFav(task.id)"
+        >favorite</i
+      >
     </div>
   </div>
 </template>
 
 <script>
+import { useTaskStore } from "../stores/TaskStore";
+
 export default {
   props: ["task"],
+  setup() {
+    const taskStore = useTaskStore();
+
+    return { taskStore };
+  },
 };
 </script>
 
@@ -43,5 +58,8 @@ export default {
   margin-left: 6px;
   cursor: pointer;
   color: #bbb;
+}
+.task i.active {
+  color: #ff005d;
 }
 </style>
